d05fa79bee4ca9aa43d50a89d69aeb0ff5c67ffe
tungluongthanh03 <tung.luongthanh03@hcmut.edu.vn>
2025-04-03 05:32:26 +0700

    new db

---
+++ backend/prisma/migrations/20250402222924_init/migration.sql
@@ -0,0 +1,76 @@
+-- CreateEnum
+CREATE TYPE "EnumDeviceType" AS ENUM ('FAN', 'LIGHT', 'WATER');
+
+-- CreateEnum
+CREATE TYPE "EnumDeviceStatus" AS ENUM ('NORMAL', 'LOSS_CONNECTION', 'OVER_THRESHOLD');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "tel" TEXT NOT NULL,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Device" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "EnumDeviceType" NOT NULL,
+    "threshold" INTEGER NOT NULL,
+    "working" BOOLEAN NOT NULL,
+    "status" "EnumDeviceStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Device_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Log_working" (
+    "id" TEXT NOT NULL,
+    "startedAt" TIMESTAMP(3) NOT NULL,
+    "endedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deviceId" TEXT NOT NULL,
+
+    CONSTRAINT "Log_working_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BlacklistedToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "BlacklistedToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "BlacklistedToken_token_key" ON "BlacklistedToken"("token");
+
+-- AddForeignKey
+ALTER TABLE "Device" ADD CONSTRAINT "Device_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Log_working" ADD CONSTRAINT "Log_working_deviceId_fkey" FOREIGN KEY ("deviceId") REFERENCES "Device"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import userRoutes from "./user.route.js";
 import authRoutes from "./auth.route.js";
 import deviceRoutes from "./device.route.js";
-import plantRoutes from "./plant.route.js";
 
 
 const router = express.Router();
@@ -10,6 +9,5 @@ const router = express.Router();
 router.use("/user", userRoutes);
 router.use("/auth", authRoutes);
 router.use("/device", deviceRoutes);
-router.use("/plant", plantRoutes);
 
 export default router;
